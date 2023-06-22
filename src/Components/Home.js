656a1c4a4878e8dd625bd6d15724ed3c3811d217
sachin <sksachinkaushik5362@gmail.com>
2023-06-22 10:36:30 +0530

    done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,16 +3,19 @@ import data from '../ContexApi'
 
 
 const Home = () => {
-  const {userdata,setUserData} =useContext(data)
-  console.log(userdata)
+  const user=JSON.parse(localStorage.getItem('user'))
+  
 
   const logout = ()=>{
-    setUserData({})
+    
   }
   return (
     <div className='container container-home'>
-        <h1>Home page</h1>
-        <h2 className="username-home">Hii 👋{userdata} </h2>
+        <h1>User Profile</h1>
+        <h2 className="username-home">Welcome {user.firstName} {user.lastName} 👋</h2>
+        <h4 className='username-home'>Email : {user.email}</h4>
+        <h5>Job Titile : {user.job}</h5>
+        <h5>Address : {user.address}</h5>
         <button className='btn' onClick={logout}>Logout</button>
     </div>
   )
